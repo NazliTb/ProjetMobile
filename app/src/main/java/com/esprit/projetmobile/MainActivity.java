2bbe35b70e28df0195f83bc049266455dc653cef
Nazli Thabet <nazli.thabet@esprit.tn>
2021-11-04 13:16:35 +0100

    Display Map and few markers
    Use and display of OSMdroid and work on cdg and orly airports as marker tests

--- app/src/main/java/com/esprit/projetmobile/MainActivity.java
+++ app/src/main/java/com/esprit/projetmobile/MainActivity.java
@@ -2,13 +2,43 @@ package com.esprit.projetmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+    SearchView search;
+    ImageButton sendButton;
+    LinearLayout cdgAirport,orlyAirport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.launch_screen);
+        search = findViewById(R.id.search);
+        sendButton = findViewById(R.id.send);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapActivity.class));
+            }
+        });
+        cdgAirport = findViewById(R.id.cdgairport);
+        cdgAirport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapActivity.class));
+            }
+        });
+        orlyAirport = findViewById(R.id.orlyairport);
+        orlyAirport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,MapActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/esprit/projetmobile/MapActivity.java
@@ -0,0 +1,69 @@
+package com.esprit.projetmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
+
+public class MapActivity extends AppCompatActivity {
+    private MapView map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        setContentView(R.layout.map_screen);
+        map = findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK); //render
+        map.setBuiltInZoomControls(true); //zoomable
+        GeoPoint startpoint = new GeoPoint(48.8566, 2.3522);
+        IMapController mapController = map.getController();
+        mapController.setZoom(12.0);
+        mapController.setCenter(startpoint);
+
+        ArrayList<OverlayItem> items = new ArrayList<>();
+        OverlayItem airport1 = new OverlayItem("CDG Airport", "Paris Charles de Gaulle Airport", new GeoPoint(49.0097, 2.5480));
+        Drawable m = airport1.getMarker(0);
+        items.add(airport1);
+        items.add(new OverlayItem("Orly Airport", "Paris Orly Airport", new GeoPoint(48.7262, 2.3652)));
+
+        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(getApplicationContext(), items, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+            @Override
+            public boolean onItemSingleTapUp(int index, OverlayItem item) {
+                return true;
+            }
+
+            @Override
+            public boolean onItemLongPress(int index, OverlayItem item) {
+                return false;
+            }
+        });
+        mOverlay.setFocusItemsOnTap(true);
+        map.getOverlays().add(mOverlay);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
+    }
+
+}
