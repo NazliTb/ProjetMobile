00b3577d491191cb756fccc1a26fa2f765e574f7
Nazli Thabet <nazli.thabet@esprit.tn>
2021-11-08 01:35:40 +0100

    some testing

--- app/src/main/java/com/esprit/projetmobile/MainActivity.java
+++ app/src/main/java/com/esprit/projetmobile/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     SearchView search;
@@ -21,12 +22,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.launch_screen);
 
         search = findViewById(R.id.search);
-        String code = search.getQuery().toString();
+        CharSequence icao = search.getQuery();
         sendButton = findViewById(R.id.send);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, TafMetarActivity.class).putExtra("key",code));
+                //Toast.makeText(getApplicationContext(),icao,Toast.LENGTH_LONG).show();
+               startActivity(new Intent(MainActivity.this, TafMetarActivity.class).putExtra("icao code",icao));
 
             }
         });
--- app/src/main/java/com/esprit/projetmobile/TafMetarActivity.java
+++ app/src/main/java/com/esprit/projetmobile/TafMetarActivity.java
@@ -2,25 +2,34 @@ package com.esprit.projetmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class TafMetarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        String icao;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tafmetar_layout);
 
+        Intent intent = getIntent();
+        icao = intent.getStringExtra("code icao");
 
-        Bundle extras = getIntent().getExtras();
+        /*Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            String code = extras.getString("key");
-        }
+            String icao = extras.getString("icao code");
+            //Toast.makeText(getApplicationContext(),icao,Toast.LENGTH_LONG);
+        }*/
 
-        String url = "https://www.getmetar.com/";
+        Toast.makeText(getApplicationContext(),icao,Toast.LENGTH_LONG).show();
+        String url = "https://www.getmetar.com/"+icao.toString();
+
         WebView webview = findViewById(R.id.mywebview);
 
         //webview.setWebViewClient(new MyWebViewClient(this));
